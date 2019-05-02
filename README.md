8ea0bedcba8920eb37e4f0e7df3d85f2146ba765
stevewhitmore <steve@swhitmore.com>
2019-05-02 15:04:06 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-v7jyfc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-v7jyfc)
---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { MatCardModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+
+import { AppRoutes } from './app.routing';
+import { AppComponent } from './app.component';
+import { BananaComponent } from './banana/banana.component';
+
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { initialState, reducers, effects } from './app.state';
+import { RotService } from './rot.service';
+import { EffectsModule } from '@ngrx/effects';
+
+@NgModule({
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    RouterModule.forRoot(AppRoutes),
+    StoreModule.forRoot(reducers, {initialState}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25
+    }),
+    EffectsModule.forRoot(effects)
+  ],
+  declarations: [ 
+    AppComponent, 
+    BananaComponent 
+  ],
+  bootstrap: [ 
+    AppComponent 
+  ]
+})
+export class AppModule { }
